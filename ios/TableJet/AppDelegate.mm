dc2dab3d3b479076fdd9eeb434896c291bb6db37
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2024-01-12 10:48:34 -0500

    Woah look, it's actually named TableJet!

--- ios/TableJet/AppDelegate.mm
+++ ios/TableJet/AppDelegate.mm
@@ -11,7 +11,7 @@
   
   [FIRApp configure];
   
-  self.moduleName = @"tbjoctosniffle";
+  self.moduleName = @"TableJet";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
